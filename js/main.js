0b5790dfa2cf22a8aa0251fa0acb5f5c599f32d8
Natalia Gilyazova <gilyazovan97@mail.ru>
2022-08-24 15:15:34 +0300

    added adaptive menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const navBtn = document.getElementById('btn__menu');
-const navMobile = document.getElementsByClassName('header__mobile')[0];
+const navMobile = document.getElementsByClassName('header__menu')[0];
+const overlay = document.getElementsByClassName('header__mobile_overlay')[0];
 const body = document.body;
 
 navBtn.addEventListener('click', (event) => {
@@ -19,5 +20,7 @@ navMobile.addEventListener('click', (event) => {
 
 function toggleMobileNav() {
     navMobile.classList.toggle('header__mobile_active');
+    navBtn.classList.toggle('nav__button_close');
+    overlay.classList.toggle('header__mobile_active');
     body.classList.toggle('no-scroll');
 }
